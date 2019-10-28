d00d4b6b26aeae579328b43e8a48c40f14a333c2
Paulo Augusto <paulo.augusto.viana@usp.br>
2019-10-28 20:30:23 -0300

    #1 images info parser

---
+++ images_defines.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 18:28:08 2019
+
+@author: Paulo
+"""
+
+JIKOS_SAVE_FOLDER = "C:/Users/Paulo/Desktop/Python shit/GarfieldNightmares/images1"
+GDOT_SAVE_FOLDER = "C:/Users/Paulo/Desktop/Python shit/GarfieldNightmares/images2"
+
+FIRST_YEAR_DOWNLOAD = 1978
+LAST_YEAR_DOWNLOAD = 2019
+YEAR_RANGE_DOWNLOAD = range(FIRST_YEAR_DOWNLOAD, LAST_YEAR_DOWNLOAD+1)
