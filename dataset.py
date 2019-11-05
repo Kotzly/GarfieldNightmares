0c49a68909cc81e241984913a0506e3911a83afb
Paulo Augusto <paulo.augusto.viana@usp.br>
2019-11-04 21:20:10 -0300

    #3 Folder redefine for load_images

--- dataset.py
+++ dataset.py
@@ -34,8 +34,8 @@ def get_info_from_filename(filename):
     year, month, day = int(year), int(month), int(day)
     return year, month, day
 
-def get_images_paths(ymd, dataset_path=GDOT_SAVE_FOLDER):
-    images = glob.glob(join(dataset_path, "**/*.png"), recursive=True)
+def get_images_paths(ymd, dataset_path=GDOT_PNG_FOLDER, ext="png"):
+    images = glob.glob(join(dataset_path, f"**/*.{ext}"), recursive=True)
     images = [image.replace("\\", "/") for image in images]
     images = [image for image in images if get_info_from_filename(image) in ymd]
     return images
@@ -65,31 +65,9 @@ def strip_image(image, mode="3"):
         k = 5
         dx = image.width//3
         w, h = image.size
+        new_size= (w//3, h)
         boxes = [[0, 0, dx+k//2, h],
                  [dx+k, 0, 2*dx-k, h],
                  [2*dx+k, 0, w, h]]
-        return [image.crop(box) for box in boxes]
-    if mode == "2x3":
-#        kx =
-        dx = image.width//3
-
-        boxes = [[0, 0, dx, h],
-                 [dx+k, 0, 2*dx-k, h],
-                 [2*dx+k, 0, w, h],
-                 [0, 0, dx, h],
-                 [dx+k, 0, 2*dx-k, h],
-                 [2*dx+k, 0, w, h]]
-    if mode == "2x4x2x3":
-        ky = 20
-    if mode == "2x4x2x3":
-            ky = 20
-    if mode == "3x3":
-        ky = 30
-        kx = 20
-        dx = image.width//3
-        dy = image.height//3
-
-        boxes [[0,0,2*dx, dy],
-               [2*dx+kx, 0, 0, w, ]]
-        return [image.crop(box) for box in boxes]
+        return [image.crop(box).resize(new_size) for box in boxes]
 
