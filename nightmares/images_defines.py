907b273aba216a08b956a9e8d8c73da85ce1b609
Paulo Augusto <paulo.augusto.viana@usp.br>
2019-12-21 23:05:34 -0300

    #7 Colab path

--- nightmares/images_defines.py
+++ nightmares/images_defines.py
@@ -10,14 +10,15 @@ from os.path import join
 ROOT_FOLDER = "C:/Users/Paulo/Desktop/Python shit/GarfieldNightmares/"
 if os.path.isdir(ROOT_FOLDER):
     print("Loading PC files...")
+    IMAGE_INFO_CSV = join(ROOT_FOLDER, "images2/images.csv")
 else:
     print("Loading Drive files...")
     ROOT_FOLDER = "/content/drive/My Drive/GarfieldNightmares"
+    IMAGE_INFO_CSV = join(ROOT_FOLDER, "png/images.csv")
 
 JIKOS_SAVE_FOLDER = join(ROOT_FOLDER, "images1")
 GDOT_SAVE_FOLDER = join(ROOT_FOLDER, "images2")
 GDOT_PNG_FOLDER = join(ROOT_FOLDER, "png")
-IMAGE_INFO_CSV = join(ROOT_FOLDER, "images2/images.csv")
 
 
 FIRST_YEAR_DOWNLOAD = 1978
